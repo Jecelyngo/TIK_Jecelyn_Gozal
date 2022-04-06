677ae95806da8f3c28b9c6f4c8199b97c331ebaa
Jecelyngo <103086000+Jecelyngo@users.noreply.github.com>
2022-04-06 11:27:41 +0800

    Create 20220406_input_output.cpp

---
+++ 20220406_input_output.cpp
@@ -0,0 +1,10 @@
+ //header
+#include <iostream>
+using namespace std;
+int main() {//begin of function
+
+    string nama;
+    cout<<"Masukan nama: "; cin>>nama;
+    cout<<"Nama anda: "<<nama;
+    
+}//end of function 
